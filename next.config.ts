4e887e73c03cb00a2c011529e409064a7db593c4
Ravi Patel <patelravi2253@gmail.com>
2025-11-06 01:58:45 +0530

    deploy 3 update

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
     ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
